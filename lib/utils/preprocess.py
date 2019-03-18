35600efdb66851e652ff3be146470ac8a74e48b0
Clarissa Cremona <cremona.clarissa@gmail.com>
2019-03-18 13:42:10 +0000

    Added custom preprocessing

--- lib/utils/preprocess.py
+++ lib/utils/preprocess.py
@@ -2,6 +2,9 @@ import re
 
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+import spelling_corr
+import contractions
+import unidecode
 
 
 def to_lower(s):
@@ -16,7 +19,7 @@ def rem_punct(s):
 
 def rem_digits(s):
     """Removes digits from a given string"""
-    return s.replace('\d+', '')
+    return re.sub('\d+', '', s)
 
 
 def rem_swords(s):
@@ -31,10 +34,33 @@ def rem_other(s):
     return " ".join([item for item in s.split() if item not in rem_words])
 
 
+def rem_accents(s):
+    """Removes accents such as é and ë"""
+    return unidecode.unidecode(s)
+
+
 def stem_words(s):
     """Performs stemming on a given string."""
     stemmer = SnowballStemmer('english')
     return " ".join([stemmer.stem(w) for w in s.split()])
+
+
+def fix_typos(s):
+    """Fixes common spelling mistakes"""
+    final = ''
+    words = s.split()
+    for w in words:
+        final += spelling_corr.correction(w) + ' '
+    return final
+
+
+def fix_contractions(s):
+    """Replaces contractions with full phrase e.g. 'won't' becomes 'will not'"""
+    return contractions.fix(s)
+
+def remove_symbols(s):
+    """Removes remaining symbols such as $,% etc."""
+    return
 
 
 def standard_preprocessing(s):
@@ -58,7 +84,9 @@ def custom_preprocessing(s, filters):
         The preprocessed string
     """
     functions_dict = {'lower': to_lower, 'punct': rem_punct, 'digits': rem_digits,
-                      'stop': rem_swords, 'custom': rem_other, 'stem': stem_words}
+                      'stop': rem_swords, 'custom': rem_other, 'stem': stem_words,
+                      'typos': fix_typos, 'contractions': fix_contractions,
+                      'accents': rem_accents}
     # Iterate through filters list and apply each function to string
     for f in filters:
         # Check that the filter is valid, otherwise print error message
@@ -103,5 +131,3 @@ def preprocess_data(data, filters=None, col=None):
             data = standard_preprocessing(data)
     return data
 
-
-function_list = [to_lower, rem_swords]
